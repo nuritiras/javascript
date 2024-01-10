5906755f9e5c769ae446a570f4d77630ac294234
Nuri TIRAŞ <47992544+nuritiras@users.noreply.github.com>
2024-01-10 23:33:24 +0300

    first commit

---
+++ js/mode.js
@@ -0,0 +1,9 @@
+function geceModu(){
+    document.getElementsByTagName("body")[0].style.color="white";
+    document.getElementsByTagName("body")[0].style.backgroundColor="black";
+}
+
+function gunduzModu(){
+    document.getElementsByTagName("body")[0].style.color="black";
+    document.getElementsByTagName("body")[0].style.backgroundColor="white";
+}
